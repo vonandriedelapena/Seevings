b831c41f3d55c83c2c094be29aa5e96240ba90ba
RudeTofu <68311570+3WokRide@users.noreply.github.com>
2025-10-19 14:07:13 +0800

    Added views and urls for Account

--- seevings/Account/urls.py
+++ seevings/Account/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 
+from .views import *
+
 urlpatterns = [
     path('', index, name='index'),
-    path('', register, name='register'),
+    path('register/', register, name='register'),
     path('login/', login, name='login'),
+    path('account/', view_account, name='account'),
+    path('account/new/', add_account, name='addAccount'),
+    path('account/update/', update_account, name='updateAccount'),
+    path('account/close/', close_account, name='closeAccount'),
 ]
