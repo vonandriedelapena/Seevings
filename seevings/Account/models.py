2b804fb3b8a3680553c13b0f85baec6784c2c9ab
choclitshake <bobownumber4@gmail.com>
2025-11-22 13:14:20 +0800

    added functionality to transaction app w/ database connectivity

--- seevings/Account/models.py
+++ seevings/Account/models.py
@@ -1,13 +1,16 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from .managers import UserManager
 
 # Create your models here.
 class User(AbstractUser):
     email = models.EmailField(_('email address'), unique=True)
     birthDate = models.DateField(null=False)
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['birthDate', 'first_name', 'last_name']
+    REQUIRED_FIELDS = ['username', 'birthDate', 'first_name', 'last_name']
+
+    objects = UserManager()
 
 class Account(models.Model):
     accountId = models.AutoField(primary_key=True)
--- seevings/Transaction/urls.py
+++ seevings/Transaction/urls.py
@@ -2,10 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.transaction_list, name='transactionList'),
+    path('', views.transaction_list_view, name='transactionList'),
 
 # AJAX Endpoints for CRUD operations
-    path('transaction/save/', views.save_transaction, name='saveTransaction'),
-    path('transaction/<int:pk>/details/', views.get_transaction_details, name='getTransactionDetails'),
-    path('transaction/<int:pk>/delete/', views.delete_transaction, name='deleteTransaction'),
+    path('load/', views.load_transactions, name='loadTransactions'),
+    path('save/', views.save_transaction, name='saveTransaction'),
+    path('<int:pk>/details/', views.get_transaction_details, name='getTransactionDetails'),
+    path('<int:pk>/delete/', views.delete_transaction, name='deleteTransaction'),
+    path('load-categories/', views.load_categories, name='loadCategories'),
+    path('load-goals/', views.load_goals, name='loadGoals'),
+    path('load-accounts/', views.load_accounts, name='loadAccounts'),
 ]
